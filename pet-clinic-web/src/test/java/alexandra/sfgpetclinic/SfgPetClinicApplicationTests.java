84ab09b362b7eabd1a6f7d1c0a387b053e1c3531
AlexandraZbant <alexandrazbant@gmail.com>
2022-02-15 19:12:49 +0200

    testing release plugin and maven release plugin

--- pet-clinic-web/src/test/java/alexandra/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/alexandra/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,14 +1,17 @@
-package sfgpetclinic;
+package alexandra.sfgpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+public class SfgPetClinicApplicationTests {
 
-
-
+    @Test
+    public void contextLoads() {
+    }
 }
+
+

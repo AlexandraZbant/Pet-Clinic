911329d82583734bd0c03a8bd38adea3f846528a
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-01 15:43:07 +0300

    added Owners and Vets List and updated Controllers. close #15, close #16

--- pet-clinic-web/src/main/java/alexandra/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/alexandra/sfgpetclinic/controllers/OwnersController.java
@@ -1,14 +1,23 @@
 package alexandra.sfgpetclinic.controllers;
 
+import alexandra.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/index", "/index.html"})
-    public String ownersList(){
+    public String ownersList(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }

3973b60b81040f58d3c92ddb95cad1dfd93493b9
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-13 18:31:21 +0300

    updated profiles for JPA service, tested active profiles, closes #48

--- pet-clinic-data/src/main/java/com/alexandra/pet_clinic/services/map/PeTypeMapService.java
+++ pet-clinic-data/src/main/java/com/alexandra/pet_clinic/services/map/PeTypeMapService.java
@@ -2,11 +2,13 @@ package com.alexandra.pet_clinic.services.map;
 
 import com.alexandra.pet_clinic.model.PetType;
 import com.alexandra.pet_clinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PeTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
     @Override
     public Set<PetType> findAll() {
--- pet-clinic-data/src/main/java/com/alexandra/pet_clinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/alexandra/pet_clinic/services/map/PetServiceMap.java
@@ -2,11 +2,13 @@ package com.alexandra.pet_clinic.services.map;
 
 import com.alexandra.pet_clinic.model.Pet;
 import com.alexandra.pet_clinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override

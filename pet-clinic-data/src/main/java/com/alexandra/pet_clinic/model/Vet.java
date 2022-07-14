cf32fe6547d974b2b40ff9ac76f248268a82023e
AlexandraZbant <alexandrazbant@gmail.com>
2022-07-14 12:16:04 +0300

    added Project Lombok to project, closes #51

--- pet-clinic-data/src/main/java/com/alexandra/pet_clinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/alexandra/pet_clinic/model/Vet.java
@@ -1,9 +1,15 @@
 package com.alexandra.pet_clinic.model;
 
+import lombok.*;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person{
@@ -12,12 +18,4 @@ public class Vet extends Person{
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
         inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialities = new HashSet<>();
-
-    public Set<Specialty> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
-    }
 }
